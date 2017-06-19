1505e2183ccd8d36b1cb5280be5da83ca276e731
camouie <camille.pellaton@students.hevs.ch>
2017-06-19 18:32:21 +0200

    add patient page

--- src/main/java/fhirtb/patientbean.java
+++ src/main/java/fhirtb/patientbean.java
@@ -2,30 +2,34 @@ package fhirtb;
 
 import java.util.ArrayList;
 import java.util.List;
-
-
+import java.util.Random;
 
 import javax.annotation.PostConstruct;
-import javax.naming.NamingException;
 
 import org.hl7.fhir.dstu3.model.Bundle;
+import org.hl7.fhir.dstu3.model.IdType;
 import org.hl7.fhir.dstu3.model.Patient;
 
 import ca.uhn.fhir.context.FhirContext;
-import ca.uhn.fhir.model.base.composite.BaseHumanNameDt;
+import ca.uhn.fhir.parser.DataFormatException;
 import ca.uhn.fhir.parser.IParser;
+import ca.uhn.fhir.rest.api.MethodOutcome;
 import ca.uhn.fhir.rest.client.IGenericClient;
 import ca.uhn.fhir.rest.client.ServerValidationModeEnum;
 
 	public class patientbean {
+		
 		public String lastname;
 		private List<Patient> patients;
-
-		public void getPatientsByLastname(){
-	    	
-	    	FhirContext ctx = FhirContext.forDstu3();
+		IGenericClient client;
+		FhirContext ctx;
+		public Patient patient;
+		
+		@PostConstruct
+		public void fhircontext () {
+			this.ctx = FhirContext.forDstu3();
 			//String serverBaseUrl = "http://sqlonfhir-stu3.azurewebsites.net/fhir";
-			String serverBaseUrl = "http://fhirtest.uhn.ca/baseDstu2";
+			String serverBaseUrl = "http://fhirtest.uhn.ca/baseDstu3";
 			
 			// Disable server validation (don't pull the server's metadata first)
 			ctx.getRestfulClientFactory().setServerValidationMode(ServerValidationModeEnum.NEVER);
@@ -35,13 +39,14 @@ import ca.uhn.fhir.rest.client.ServerValidationModeEnum;
 	        ctx.getRestfulClientFactory().setSocketTimeout(60 * 1000);
 	        
 	        // create the RESTful client to work with our FHIR server 
-	        IGenericClient client = ctx.newRestfulGenericClient(serverBaseUrl);
-			
-	        
+	        this.client = ctx.newRestfulGenericClient(serverBaseUrl);
 
+		}
+
+		public void getPatientsByLastname(){
 	        try {
 	            // search for the resource created
-	            Bundle response = client.search()
+	            Bundle response = this.client.search()
 	                    .forResource(Patient.class)
 	                    .where(Patient.FAMILY.matches().values(this.lastname))
 	                    .returnBundle(Bundle.class)
@@ -54,14 +59,14 @@ import ca.uhn.fhir.rest.client.ServerValidationModeEnum;
 	            this.patients = new ArrayList<Patient>();
 	            
 	            response.getEntry().forEach((entry) -> {
-	            	IParser jParser = ctx.newJsonParser().setPrettyPrint(true);
+	            	IParser jParser = this.ctx.newJsonParser().setPrettyPrint(true);
 	            	String resourceJSON = jParser.encodeResourceToString(entry.getResource());
 	            	System.out.println(resourceJSON);
 	            	
 	            	//populate the list with the retrieved bundle's resources
 	            	Patient p = (Patient) entry.getResource();
 	            	this.patients.add(p);
-	            	System.out.println(p.getNameFirstRep().getNameAsSingleString());
+	            	System.out.println(p.getNameFirstRep().getFamily());
 	            });
 	            
 	        } catch (Exception e) {
@@ -71,9 +76,52 @@ import ca.uhn.fhir.rest.client.ServerValidationModeEnum;
 	    	
 	    }
 		
+		public void addPatient(String firstname, String lastname){
+			
+	        
+	        this.patient = new Patient();
+			
+			 Random randomGenerator = new Random();
+			 int randomInt = randomGenerator.nextInt(10000000);
+		
+			 String prefix = "Mr";
+			 
+	        this.patient.addName().addPrefix(prefix).setFamily(lastname).addGiven(firstname); 
+	        this.patient.addIdentifier()
+	                .setSystem("tb:fhir")
+	                .setValue("CP"+randomInt);
+	        
+	        try {
+	            MethodOutcome outcome = this.client.create()
+	                    .resource(this.patient)
+	                    .prettyPrint()
+	                    .encodedJson()
+	                    .execute();
+
+	            IdType id = (IdType) outcome.getId();
+	            System.out.println("Resource is available at: " + id.getValue());
+
+	            
+	        } catch (DataFormatException e) {
+	            System.out.println("An error occurred trying to upload:");
+	            e.printStackTrace();
+	        }
+
+			
+		}
+		
 		public void setLastname(String lastname) {this.lastname = lastname;}
 		public String getLastname() {return this.lastname;}
 		public List<Patient> getPatients() {return patients;}
 		public void setPatients(List<Patient> patients) {this.patients = patients;}
+
+		public Patient getPatient() {
+			return patient;
+		}
+
+		public void setPatient(Patient patient) {
+			this.patient = patient;
+		}
+		
 
 }

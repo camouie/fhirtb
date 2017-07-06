8374a99b7835ecb16d9ab4974bb6a3a97f0f9984
camouie <camille.pellaton@students.hevs.ch>
2017-07-06 14:52:35 +0200

    getting doctor list from DB and from server to populate dropdown list
    in addpatient view

--- src/main/java/fhirtb/patientbean.java
+++ src/main/java/fhirtb/patientbean.java
@@ -9,20 +9,28 @@ import javax.annotation.PostConstruct;
 
 import org.hl7.fhir.dstu3.model.Bundle;
 import org.hl7.fhir.dstu3.model.IdType;
+import org.hl7.fhir.dstu3.model.Observation;
 import org.hl7.fhir.dstu3.model.Patient;
+import org.hl7.fhir.dstu3.model.Practitioner;
+import org.hl7.fhir.dstu3.model.Reference;
 
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.parser.DataFormatException;
 import ca.uhn.fhir.parser.IParser;
 import ca.uhn.fhir.rest.api.MethodOutcome;
 import ca.uhn.fhir.rest.client.IGenericClient;
+import ca.uhn.fhir.rest.gclient.TokenClientParam;
 
 public class patientbean {
 
 	private String lastname;
 	private List<Patient> patients;
 	private Patient patient;
+	private Practitioner doctor;
 	private Date birthdate;
+	private List<Practitioner> doctors;
+	private String doctorid;
+	
 	private String serverBaseUrl;
 	private FhirContext ctx;
 	private Fhircontextconnection fco;
@@ -36,7 +44,14 @@ public class patientbean {
 		
 		//initialize the patients list
 		this.patients = new ArrayList<Patient>();
+		this.doctors = new ArrayList<Practitioner>();
 
+	}
+	/*
+	 * called by prerenderview method on page addpatient
+	 */
+	public void load() throws ClassNotFoundException{
+		this.getDoctorsForPatient();
 	}
 
 	/*
@@ -91,6 +106,10 @@ public class patientbean {
 		this.patient.addName().addPrefix(prefix).setFamily(lastname).addGiven(firstname);
 		this.patient.setBirthDate(this.birthdate);
 		this.patient.addIdentifier().setSystem("tb:fhir").setValue("CP" + randomInt);
+		
+		//get the selected doctor on the page from the fhir server
+		this.getSelectedDoctorbyID();
+		this.patient.addGeneralPractitioner(new Reference(this.doctor));
 
 		try {
 			MethodOutcome outcome = client.create().resource(this.patient).prettyPrint().encodedJson().execute();
@@ -104,6 +123,62 @@ public class patientbean {
 		}
 
 	}
+	public void getDoctorsForPatient() throws ClassNotFoundException {
+		// create the RESTful client to work with our FHIR server
+		IGenericClient client = ctx.newRestfulGenericClient(serverBaseUrl);
+		ArrayList<String> doctorsid = new ArrayList<String>();
+		doctorsid = DAO.getPractitioners();
+		
+		for (String doctorid : doctorsid)
+		{
+		try {
+			Bundle response = client.search().forResource(Practitioner.class)
+					.where(new TokenClientParam("_id").exactly().code(doctorid))
+					.prettyPrint()
+					.returnBundle(Bundle.class)
+					.execute();
+
+			System.out.println("Found " + response.getTotal() + " doctor with ID " + doctorid);
+
+			if (response.getEntry().size() != 0) {
+				Practitioner p = (Practitioner) response.getEntry().get(0).getResource();
+				System.out.println("adding doctor from server to bean list with uname = " + p.getCommunicationFirstRep());
+				
+				this.doctors.add(p);
+			}
+
+		} catch (Exception e) {
+			System.out.println("An error occurred trying to search:");
+			e.printStackTrace();
+		}
+		}
+
+	}
+	
+	public void getSelectedDoctorbyID(){
+		IGenericClient client = ctx.newRestfulGenericClient(serverBaseUrl);
+		try {
+			Bundle response = client.search().forResource(Practitioner.class)
+					.where(new TokenClientParam("identifier").exactly().code(this.doctorid))
+					.prettyPrint()
+					.returnBundle(Bundle.class)
+					.execute();
+
+			System.out.println("Found the selected doctor on the server for patient. doctor id : " + doctorid);
+
+			if (response.getEntry().size() != 0) {
+				Practitioner p = (Practitioner) response.getEntry().get(0).getResource();
+				System.out.println("adding doctor from server to bean list with uname = " + p.getCommunicationFirstRep());
+				
+				this.setDoctor(p);
+			}
+
+		} catch (Exception e) {
+			System.out.println("An error occurred trying to search:");
+			e.printStackTrace();
+		}
+	}
+	
 	/*
 	 * Getters and setters methods
 	 */
@@ -140,4 +215,23 @@ public class patientbean {
 		this.birthdate = birthdate;
 	}
 
+	public List<Practitioner> getDoctors() {
+		return doctors;
+	}
+
+	public void setDoctors(List<Practitioner> doctors) {
+		this.doctors = doctors;
+	}
+	public String getDoctorid() {
+		return doctorid;
+	}
+	public void setDoctorid(String doctorid) {
+		this.doctorid = doctorid;
+	}
+	public void setDoctor(Practitioner doctor) {
+		this.doctor = doctor;
+	}
+	
+	
+	
 }
